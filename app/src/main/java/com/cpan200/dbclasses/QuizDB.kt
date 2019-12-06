cc7a6c184b4dad9d2630a78b00f4859fd7015b6d
uncleankiwi <feldtspirit@gmail.com>
2019-12-06 08:42:14 -0500

    -

--- app/src/main/java/com/cpan200/dbclasses/QuizDB.kt
+++ app/src/main/java/com/cpan200/dbclasses/QuizDB.kt
@@ -1,4 +1,48 @@
 package com.cpan200.dbclasses
 
-class QuizDB {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class QuizDB(
+    context: Context?,
+    quizID: Int,
+    factory: SQLiteDatabase.CursorFactory?
+    //version: Int
+) : SQLiteOpenHelper(context, "$filenameA$quizID$filenameB", factory, version) {
+    companion object{
+        private const val version = 1
+        private const val filenameA = "Quiz"
+        private const val filenameB = ".db"
+        const val COL_ID = "_id"
+        const val COL_QUESTION = "question"
+        const val COL_ANS_N = "ans"             //choices: ans1, ans2, ans3, etc
+        const val COL_MARKS = "marks"
+        const val COL_CORRECTANS = "correctans"
+        const val COL_IMAGE = "image"             //picture associated with this question
+    }
+
+    val tableName = "quiz${quizID}"
+
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        //questions, students, quiz IDs all begin with 0 in storage
+        //in actual use, 1 is added
+        db?.execSQL("CREATE TABLE $tableName (" +
+                "$COL_ID INTEGER PRIMARY KEY, " +
+                "$COL_QUESTION TEXT, " +
+                "${COL_ANS_N}0 INT, " +
+                "${COL_ANS_N}1 INT, " +
+                "${COL_ANS_N}2 INT, " +
+                "${COL_ANS_N}3 INT, " +
+                "${COL_ANS_N}4 INT, " +
+                "$COL_MARKS REAL, " +
+                "$COL_CORRECTANS INT, " +
+                "$COL_IMAGE BLOB)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS $tableName")
+        onCreate(db)
+    }
 }
--- app/src/main/java/com/cpan200/dbclasses/QuizzesDB.kt
+++ app/src/main/java/com/cpan200/dbclasses/QuizzesDB.kt
@@ -1,4 +1,43 @@
 package com.cpan200.dbclasses
 
-class QuizzesDB {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class QuizzesDB(
+    context: Context?,
+    //name: String?,
+    factory: SQLiteDatabase.CursorFactory?
+    //version: Int
+) : SQLiteOpenHelper(context, filename, factory, version) {
+    companion object{
+        private const val version = 1
+        private const val filename = "Quizzes.db"
+        const val TABLE_NAME = "quizzes"
+        const val COL_ID = "_id"
+        const val COL_TITLE = "title"
+        const val COL_QUESTIONS = "questions"
+        const val COL_TOTALMARKS = "totalmarks"
+        const val COL_FINALIZED = "finalized"       //ready to publish?
+        const val COL_MAXOPTIONS = "maxoptions"     //highest number of choices in an MCQ
+        const val COL_MAXATTEMPTS = "maxattempts"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE $TABLE_NAME (" +
+                "$COL_ID INTEGER PRIMARY KEY, " +
+                "$COL_TITLE TEXT, " +
+                "$COL_QUESTIONS INT, " +
+                "$COL_TOTALMARKS REAL, " +
+                "$COL_FINALIZED INT, " +
+                "$COL_MAXOPTIONS INT, " +
+                "$COL_MAXATTEMPTS INT)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+
+
 }
