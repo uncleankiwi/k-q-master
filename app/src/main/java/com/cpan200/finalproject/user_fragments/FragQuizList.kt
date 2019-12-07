edb82273ba6a494ca1445fe3a3b7502b91bd2910
uncleankiwi <feldtspirit@gmail.com>
2019-12-07 03:14:14 -0500

    -

---
+++ app/src/main/java/com/cpan200/finalproject/user_fragments/FragQuizList.kt
@@ -0,0 +1,25 @@
+package com.cpan200.finalproject.user_fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cpan200.finalproject.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragQuizList : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_quiz_list, container, false)
+    }
+
+
+}
